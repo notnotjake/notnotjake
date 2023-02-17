b2a67957fc6eb45f80f20fa06afebbbf963727f2
notnotjake <jake@notnotjake.com>
2023-02-16 21:44:12 -0500

    Updates to 11ty 2.0 beta
    also moves index js to assets/js/pages/index.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,51 +1,53 @@
 const {DateTime} = require('luxon')
 
+
 module.exports = function(eleventyConfig) {
-	// Watch CSS files for changes
-	eleventyConfig.setBrowserSyncConfig({
-	  files: './public/css/**/*.css'
+	/* Watch CSS & JS files for changes
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+	eleventyConfig.setServerOptions({
+		watch: ['./public/css/**/*.css'],
+		showAllHosts: true
+	})
+	/* Passthrough JavaScript folder
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+	eleventyConfig.addPassthroughCopy({
+		"source/_assets/js": "js"
 	})
 	
-	// Dev/Prod Linking
 	
-	// the goal here is that i have a function which knows if running in prod or dev and then i give a relative link to the asset. then it completes the url based on the environment and based on the type of asset (large-assets or just assets)
+	/* SHORTCODE: Prettify Page Dates
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+	eleventyConfig.addShortcode("prettyDate", ( pageDate ) => {
+		return DateTime.fromJSDate(pageDate, {zone: 'America/New_York'}).toLocaleString(DateTime.DATE_MED)
+	})
+	/* SHORTCODE: Current Year
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+	eleventyConfig.addShortcode("year", () => {
+		return `${new Date().getFullYear()}`
+	})
 	
-	// to determine environment, environment variables will be used. the function will look at this var to determine which to use.
-	// env vars will be set via the npm script. the server will use build:prod instead of build or run script which will pass the prod variable to this function to replace links
+
+	/* Environment Production|Dev
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 	module.exports = function() {
 		return {
 			environment: process.env.MY_ENVIRONMENT || "dev"
 		}
 	}
-	
-	/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	Limits the number of objects returned in a collection
+	/* SHORTCODE: Asset Links
 	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-	eleventyConfig.addFilter("limit", (array, limit) => {
-		return array.slice(0, limit)
-	})
-	// Sample Use: {%- for post in collections.posts reversed | limit(3) -%}
-	
 	eleventyConfig.addShortcode("asset", (linkRelative, assetHost) => {
-        if ( process.env.MY_ENVIRONMENT === "prod" ) {
-            return `${assetHost}/${linkRelative}`
-        }
-        else {
-            return `../_assets/${linkRelative}`
-        }
+		if ( process.env.MY_ENVIRONMENT === "prod" ) {
+			return `${assetHost}/${linkRelative}`
+		}
+		else {
+			return `../_assets/${linkRelative}`
+		}
 	})
 	
-	// Convert the page date to a nicer format
-	eleventyConfig.addShortcode("prettyDate", ( pageDate ) => {
-		return DateTime.fromJSDate(pageDate, {zone: 'America/New_York'}).toLocaleString(DateTime.DATE_MED)
-	})
 	
-	// Returns the current year at time of build
-	eleventyConfig.addShortcode("year", () => {
-		return `${new Date().getFullYear()}`
-	})
-	
-	// Return your Object options:
+	/* Set input/output
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 	return {
 		dir: {
 			input: "source",

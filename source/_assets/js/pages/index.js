91ee775815b0360d7c4daeebf2ea46d7458596e0
notnotjake <jake@notnotjake.com>
2023-04-25 16:34:39 -0400

    improves splash animation
    should fix the flash that sometimes occured before animation and generally be smoother.

--- source/_assets/js/pages/index.js
+++ source/_assets/js/pages/index.js
@@ -20,7 +20,7 @@ else {
 	
 	/* Scroll Hint Bounce (after 3.5s if under 10px scrolled)
 	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-	gsap.delayedCall(3.1, () => {
+	gsap.delayedCall(4.0, () => {
 		if ( document.documentElement.scrollTop < 10 && !document.querySelector('#splash').classList.contains('hide') && !prefersReducedMotion ) {
 			// Scroll bounce
 			gsap.to(window, {
@@ -95,93 +95,6 @@ else {
 /* Splash [End] */
 
 
-
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Nav, Marquee
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-// let headerScrollTimelineDefault = ''
-// if ( !prefersReducedMotion ) {
-// 	/* Only continues if Prefers-Reduced-Motion is OFF
-// 	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-// 	
-// 	document.querySelector('#scroll-collection-2').classList.remove('hide')
-// 	
-// 	const header = document.querySelector('nav')
-// 	const headerScroller = document.querySelector('nav ul')
-// 	const headerContent1 = document.querySelector('#scroll-collection-1')
-// 	const headerContent2 = document.querySelector('#scroll-collection-2')
-// 	
-// 	const speed = 60 // Set the horiz. scroll speed for marquee
-// 	const scrollEnd = headerContent1.offsetWidth + headerContent2.offsetWidth
-// 	let scrollDuration = scrollEnd / speed
-// 
-// 	
-// 	/* On Load: Start scroll
-// 	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-// 	// On load: start scrolling to end, after 2s delay
-// 	headerScrollTimelineDefault = gsap.timeline()
-// 	headerScrollTimelineDefault.to(headerScroller, {
-// 		scrollTo: {y:0, x: 20, autoKill: true},
-// 		duration: 1.9,
-// 		delay: 1.9,
-// 		ease: 'power3.in'
-// 	})
-// 	headerScrollTimelineDefault.to(headerScroller, {
-// 		scrollTo: {y: 0, x: scrollEnd, autoKill: true},
-// 		duration: scrollDuration,
-// 		ease: 'linear'
-// 	})
-// 	headerScrollTimelineDefault.pause()
-// 	
-// 	let headerScrollControl = gsap.timeline() // Used by mouse enter/leave to reset the animation
-// 	
-// 	/* Hover: stop scrolling with ease-out
-// 	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-// 	header.addEventListener('mouseenter', () => {		
-// 		headerScrollTimelineDefault.kill()
-// 		
-// 		headerScrollControl.kill()
-// 		headerScrollControl.clear()
-// 		
-// 		let scrollEase = headerScroller.scrollLeft + 10
-// 		headerScrollControl.to(headerScroller, {
-// 				scrollTo: {x: scrollEase, y: 0, autoKill: true},
-// 				duration: 0.8,
-// 				ease: 'power3.out'
-// 			})
-// 		headerScrollControl.play()
-// 	})
-// 	/* Hover-Out: start up new scroller
-// 	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-// 	headerScroller.addEventListener('mouseleave', () => {		
-// 		headerScrollControl.kill()
-// 		headerScrollControl.clear()
-// 		
-// 		let scrollDistanceToEnd = scrollEnd - headerScroller.scrollLeft
-// 		scrollDuration = scrollDistanceToEnd / speed
-// 		
-// 		headerScrollControl.to(headerScroller, {
-// 			scrollTo: {x: scrollEnd, y: 0, autoKill: true},
-// 			duration: scrollDuration,
-// 			ease: 'linear'
-// 		})
-// 		headerScrollControl.play()
-// 	})
-// 	/* Finger-Up: when touch event ends, start up new scroller
-// 	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-// 	headerScroller.addEventListener('touchend', () => {
-// 		let scrollDistanceToEnd = scrollEnd - headerScroller.scrollLeft
-// 		scrollDuration = scrollDistanceToEnd / speed
-// 		
-// 		gsap.delayedCall(0.5, () => {
-// 			gsap.to(headerScroller, {
-// 				scrollTo: {x: scrollEnd, y: 0, autoKill: true},
-// 				duration: scrollDuration,
-// 				ease: 'linear'
-// 			})
-// 		})
-// 	})
-// }
 
 function startNavScroll () {
 	document.querySelector('#scroll-collection-2').classList.remove('hide')

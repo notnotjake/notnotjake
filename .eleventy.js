26ba184c9659a5f3c73e76a09410173fa7a7f0ec
notnotjake <jake@notnotjake.com>
2023-08-25 03:56:38 -0400

    trying to sort posts collection by year

--- .eleventy.js
+++ .eleventy.js
@@ -19,41 +19,6 @@ module.exports = function(eleventyConfig) {
 			"static": "static"
 		})
 	}
-	
-	
-	/* SHORTCODE: Prettify Page Dates
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-	eleventyConfig.addShortcode("prettyDate", function ( pageDate ) {
-		let d = DateTime.fromJSDate(pageDate, {zone: 'America/New_York'}).toLocaleString(DateTime.DATE_MED)
-		return d
-	})
-	/* SHORTCODE: Prettify Page Dates SHORT
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-	eleventyConfig.addFilter("prettyDateShort", function( xDate ) {
-		
-		let d = DateTime.fromJSDate(xDate, {zone: 'America/New_York'})
-		d = d.toFormat('MMM d')
-		return d
-		
-	})
-	/* SHORTCODE: Current Year
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-	eleventyConfig.addShortcode("year", () => {
-		return `${new Date().getFullYear()}`
-	})
-	
-	/* FILTER: Time to Read
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-	eleventyConfig.addFilter("timeToRead", function ( content ) {
-		content = content.replace(/(<([^>]+)>)/gi, '')
-		let matches = content.match(/[\u0400-\u04FF]+|\S+\s*/g)
-		let count = matches !== null ? matches.length : 0
-		
-		const wpm = 200
-		let mins = Math.round( count / wpm )
-		return mins.toString()
-	})
-
 	/* Environment Production|Dev
 	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 	module.exports = function() {
@@ -71,10 +36,79 @@ module.exports = function(eleventyConfig) {
 		else {
 			return `/static/${linkRelative}`
 		}
+	})	
+	
+	
+	/* SHORTCODE: Prettify Page Dates
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+	eleventyConfig.addShortcode("prettyDate", function ( pageDate ) {
+		let d = DateTime.fromJSDate(pageDate, {zone: 'America/New_York'}).toLocaleString(DateTime.DATE_MED)
+		return d
 	})
-	
-	
-	
+	/* SHORTCODE: Prettify Page Dates SHORT
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+	eleventyConfig.addFilter("prettyDateShort", function( xDate ) {
+		let d = DateTime.fromJSDate(xDate, {zone: 'America/New_York'})
+		d = d.toFormat('MMM d')
+		return d
+	})
+	/* SHORTCODE: Page Year
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+	eleventyConfig.addFilter("pageYear", function( xDate ) {
+		let d = DateTime.fromJSDate(xDate, {zone: 'America/New_York'})
+		d = d.toFormat('yyyy')
+		return d
+	})
+	/* SHORTCODE: Current Year
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+	eleventyConfig.addShortcode("year", () => {
+		return `${new Date().getFullYear()}`
+	})
+	/* FILTER: Time to Read
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+	eleventyConfig.addFilter("timeToRead", function ( content ) {
+		content = content.replace(/(<([^>]+)>)/gi, '')
+		let matches = content.match(/[\u0400-\u04FF]+|\S+\s*/g)
+		let count = matches !== null ? matches.length : 0
+		
+		const wpm = 200
+		let mins = Math.round( count / wpm )
+		return mins.toString()
+	})
+	/* COLLECTION: Posts by Year
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+	eleventyConfig.addCollection("postsByYear", (collection) => {
+		let postsByYear = {}
+		let posts = collection.getFilteredByGlob(["./source/posts/**/*.md"])
+		posts.forEach( i => {
+			let y =i.data.year
+			if (!postsByYear[y]) {
+				postsByYear[y] = new Array()
+			}
+			postsByYear[y].push( i )
+		})
+		console.log(postsByYear)
+		return postsByYear
+	});
+	/* COLLECTION: List of Series
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+	eleventyConfig.addCollection("allSeries", (collection) => {
+		let series = {}
+		let posts = collection.getFilteredByGlob(["./source/posts/**/*.md"])
+		posts.forEach( p => {
+			if (p.data.series) {
+				let s = p.data.series
+				if (!series[s]) {
+					series[s] = new Array()
+				}
+				series[s].push( p )
+			}
+		})
+		console.log(series)
+		return series
+	});
+
+
 	/* Set input/output
 	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 	return {
